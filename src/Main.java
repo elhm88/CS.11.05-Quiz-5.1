64e84d29857f1c3bee304a23d6b55e56366d23ef
elhm88 <elhm88@student.cis.edu.hk>
2023-12-05 15:02:43 +0800

    Quiz 5 (IDE).

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,16 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
+        int[] array = new int[array1.length + array2.length];
 
-        return null;
+        for (int i=0; i < array1.length; i++){
+            array[i] = array1[i];
+        }
 
+        for (int i=0; i < array2.length; i++){
+            array[i+array1.length] = array2[i];
+        }
+        return array;
     }
 
     /**
@@ -21,9 +28,17 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
+        int[] array = new int[array1.length + array2.length];
+        int num = 0;
 
-        return null;
+        for (int i=0; i < array1.length; i++){
+            array[num] = array1[i];
+            num++;
 
+            array[num] = array2[i];
+            num++;
+        }
+        return array;
     }
 
     /**
@@ -34,8 +49,10 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
+        for (int i=0; i < array1.length; i++){
+            array1[i] = array1[i] * array2[i];
+        }
+        return array1;
 
     }
 
@@ -56,15 +73,21 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
-
+        int[] count = new int[words.length];
+        for (int i = 0; i < words.length ; i++) {
+            count[i] = countCapitalLetters(words[i]);
+        }
+        return count;
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
-
+        int count = 0;
+        for (int i = 0; i < word.length(); i++){
+            if (word.charAt(i)>='A' && word.charAt(i)<='Z'){
+                count++;
+            }
+        }
+        return count;
     }
 
 }
